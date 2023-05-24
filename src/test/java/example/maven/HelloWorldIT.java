66a84bf7a8be63a913a8b7f2a7e7dcbc6e88c1e3
ghparagas <gilwen.h.paragas@gmail.com>
2023-05-24 09:40:50 +0800

    added integration testing

---
+++ src/test/java/example/maven/HelloWorldIT.java
@@ -0,0 +1,14 @@
+package example.maven;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldIT {
+
+    @Test
+    void integrationTest() {
+
+        System.out.println("Running integration testing");
+
+    }
+
+}
